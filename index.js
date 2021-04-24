161c1a7923c224f236cdcf0c73a4287dbe135ba0
Arsalan Khattak <me@khattak.dev>
2021-04-24 23:40:14 +0500

    improved presentation

--- index.js
+++ index.js
@@ -2,15 +2,32 @@
 
 const { default: axios } = require("axios");
 const readline = require("readline");
+const Table = require("cli-table");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 rl.question("What's your GitHub Handle?", async function (answer) {
-  const profileInfo = await axios.get(`https://api.github.com/users/${answer}`);
+  try {
+    var profileInfo = await axios.get(`https://api.github.com/users/${answer}`);
+  } catch (error) {
+    console.log("Something went unexpected!");
+  }
 
-  console.log(profileInfo.data);
+  const { name, company, location, followers, following } = profileInfo.data;
+
+  const profileTable = new Table();
+
+  profileTable.push(
+    { Name: name || "" },
+    { Company: company || "" },
+    { Location: location || "" },
+    { Followers: followers || "" },
+    { Following: following || "" }
+  );
+
+  console.log(profileTable.toString());
 
   rl.close();
 });
