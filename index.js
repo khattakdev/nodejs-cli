c3ca2d4cb6f01c31fb0526131b6c77a0dc521ebb
Arsalan Khattak <me@khattak.dev>
2021-04-24 23:47:24 +0500

    show orgs

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ rl.question("What's your GitHub Handle?", async function (answer) {
   const { name, company, location, followers, following } = profileInfo.data;
 
   const profileTable = new Table();
+  const orgTable = new Table({
+    head: ["Organization", "Description"],
+  });
 
   profileTable.push(
     { Name: name || "" },
@@ -27,7 +30,20 @@ rl.question("What's your GitHub Handle?", async function (answer) {
     { Following: following || "" }
   );
 
+  try {
+    var orgInfo = await axios.get(
+      `https://api.github.com/users/${answer}/orgs`
+    );
+  } catch (error) {
+    console.log("Something went unexpected!");
+  }
+
+  orgInfo.data.forEach((org) => {
+    orgTable.push([org.login, org.description.slice(0, 50) + "..."]);
+  });
+
   console.log(profileTable.toString());
+  console.log(orgTable.toString());
 
   rl.close();
 });
